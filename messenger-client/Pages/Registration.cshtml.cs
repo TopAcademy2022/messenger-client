aa22cea67bd1c1d68b3d42c19c38aefd2a9247ad
Kasyanov-Igor <igor.kasyanov00@mail.ru>
2024-08-07 21:42:42 +0300

    Delit injection

--- messenger-client/Pages/Registration.cshtml.cs
+++ messenger-client/Pages/Registration.cshtml.cs
@@ -1,12 +1,39 @@
-using Microsoft.AspNetCore.Mvc;
+using messenger_client.Services;
+using messenger_client.Services.Interfeces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace messenger_client.Pages
 {
     public class RegistrationModel : PageModel
     {
-        public void OnGet()
+        IBlockInjections blockInjections = new BlockInjections();
+
+		public void OnGet(bool regRes)
+		{
+			if (regRes)
+			{
+				Response.Redirect("/Login");
+			}
+			
+		}
+
+		public void OnPost()
         {
-        }
+			string? login = Request.Form["login"];
+			string? password = Request.Form["password"];
+			string? email = Request.Form["email"];
+
+			if (blockInjections.CheckForProhibitedSymbol(login)&& 
+				blockInjections.CheckForProhibitedSymbol(password)&& 
+				blockInjections.CheckForProhibitedSymbol(email))
+			{
+				
+				Response.Redirect("/Login");
+			}
+			else
+			{
+				Response.Redirect("/Registration");
+			}
+		}
     }
 }
